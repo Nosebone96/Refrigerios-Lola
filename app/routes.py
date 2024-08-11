adeb04e240e9de76b92d82f54dbcd6527fd5ccba
Nosebone96 <140755097+Nosebone96@users.noreply.github.com>
2024-08-11 14:58:05 -0500

    si, hola

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,11 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template('main_html.html')
+
 @bp.route('/complemento')
 def complemento():
     return render_template('complemento.html')
+
 @bp.route('/vaso_de_leche')
 def vaso_de_leche():
     return render_template('vaso_de_leche.html')
